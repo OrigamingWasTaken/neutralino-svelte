2a9d19ab2e7ed2bdd4f0d63cb873d354dc31a5c0
OrigamingWasTaken <74014262+OrigamingWasTaken@users.noreply.github.com>
2024-02-22 16:34:27 +0100

    Added Neutralino download

--- scripts/package/dev.ts
+++ scripts/package/dev.ts
@@ -1,33 +1,47 @@
 import {spawn} from 'child_process';
 import path from 'path';
+import {existsSync, chmodSync} from 'fs';
 
-let binaryOS = 'linux';
-switch (process.platform) {
-	case 'darwin':
-		binaryOS = 'mac';
-		break;
-	case 'win32':
-		binaryOS = 'win';
-		break;
+function main() {
+	let binaryOS = 'linux';
+	switch (process.platform) {
+		case 'darwin':
+			binaryOS = 'mac';
+			break;
+		case 'win32':
+			binaryOS = 'win';
+			break;
+	}
+	const vite = spawn('vite', {
+		cwd: process.cwd(),
+		detached: false,
+		stdio: 'inherit',
+	});
+	const args = [
+		'--window-enable-inspector=true',
+		'--export-auth-info',
+		'--load-dir-res',
+		`--path=${path.resolve('.')}`,
+		'--neu-dev-extension',
+		'--url=http://localhost:5173',
+	];
+	chmodSync(`./bin/neutralino-${binaryOS}_${process.arch}`,"755")
+	const neu = spawn(`./bin/neutralino-${binaryOS}_${process.arch}`, args, {
+		cwd: process.cwd(),
+		detached: false,
+		stdio: 'inherit',
+	}).on('close', () => {
+		vite.kill();
+		process.exit();
+	});
 }
-const vite = spawn('vite', {
-	cwd: process.cwd(),
-	detached: false,
-	stdio: 'inherit',
-});
-const args = [
-	'--window-enable-inspector=true',
-	'--export-auth-info',
-	'--load-dir-res',
-	`--path=${path.resolve('.')}`,
-	'--neu-dev-extension',
-	'--url=http://localhost:5173',
-];
-const neu = spawn(`./bin/neutralino-${binaryOS}_${process.arch}`, args, {
-	cwd: process.cwd(),
-	detached: false,
-	stdio: 'inherit',
-}).on('close', () => {
-	vite.kill();
-	process.exit();
-});
+
+if (!existsSync(path.resolve('./bin'))) {
+	const neuInstall = spawn('npx', ['neu', 'update'], {
+		cwd: process.cwd(),
+		detached: false,
+		stdio: 'inherit',
+	}).on('exit', main);
+} else {
+	main();
+}
---
+++ scripts/package/releases.ts
@@ -0,0 +1,5 @@
+import { Signale } from "signale";
+
+const logger = new Signale()
+
+logger.start("Building as a release")
